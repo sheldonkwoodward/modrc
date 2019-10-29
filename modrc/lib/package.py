468c0dcc9815646bbd269d17fc63e889f2f4de6c
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-10-28 23:19:35 -0700

    Added package module to lib with tests.

---
+++ modrc/lib/package.py
@@ -0,0 +1,31 @@
+from modrc.lib import helper
+
+def create_package(modrc_dir, package_name):
+    """Create a new ModRC package.
+
+    Parameters
+    ----------
+    modrc_dir : :obj:`Path`
+        The ModRC directory.
+    package_name : str
+        The name of the new package.
+
+    Returns
+    -------
+    :obj:`Path`
+        The path to the new package.
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if either the ModRC or packages directory do not exist.
+    """
+    # get the packges directory
+    packages_dir = helper.get_packages_dir(modrc_dir)
+    # define the package directories and files
+    new_package_dir = packages_dir.joinpath(package_name)
+    new_package_yml = new_package_dir.joinpath('package.yml')
+    # create the package directories and files
+    new_package_dir.mkdir()
+    new_package_yml.touch()
+    return new_package_dir
---
+++ modrc/tests/lib/test_package.py
@@ -0,0 +1,31 @@
+import pathlib
+import tempfile
+import unittest
+
+from modrc.lib import package, setup
+
+
+class TestCreatePackage(unittest.TestCase):
+    def setUp(self):
+        # setup a temporary mock home directory
+        self.temp = tempfile.TemporaryDirectory()
+        self.parent_dir = pathlib.Path(self.temp.name)
+        self.modrc_dir = setup.create_modrc_directory(self.parent_dir)
+        self.packages_dir = self.modrc_dir.joinpath('packages')
+
+    def tearDown(self):
+        self.temp.cleanup()
+
+    def test_create_package_dir(self):
+        """Test the creation of a new package directory."""
+        new_package_name = 'test-package'
+        new_package_dir = package.create_package(self.modrc_dir, new_package_name)
+        self.assertEqual(new_package_dir, self.packages_dir.joinpath(new_package_name))
+        self.assertTrue(new_package_dir.exists())
+
+    def test_create_package_yml(self):
+        """Test the creation of the package.yml file"""
+        new_package_name = 'test-package'
+        new_package_dir = package.create_package(self.modrc_dir, new_package_name)
+        new_package_yml = new_package_dir.joinpath('package.yml')
+        self.assertTrue(new_package_yml.is_file())
