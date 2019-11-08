fad5cf0ce0cabfc1c0041e9317b863d909ec607a
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-11-07 19:27:00 -0800

    Added helper methods in the package module for getting packages and creating files.

--- modrc/lib/package.py
+++ modrc/lib/package.py
@@ -27,3 +27,63 @@ def create_package(package_name):
     new_package_dir.mkdir()
     new_package_yml.touch()
     return new_package_dir
+
+def get_package(package_name):
+    """Get a package by name.
+
+    Parameters
+    ----------
+    package_name : str
+        The name of the package to get.
+
+    Returns
+    -------
+    :obj:`Path`
+        The path the package
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if the package does not exist.
+    """
+    packages_dir = helper.get_packages_dir()
+    new_package_dir = packages_dir.joinpath(package_name)
+    if not new_package_dir.is_dir():
+        raise FileNotFoundError('Package does not exist')
+    return new_package_dir
+
+def create_file(file_name, package_name):
+    """Create a new file in a package.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to create.
+    package_name : str
+        The name of the package that the file will go in.
+
+    Returns
+    -------
+    :obj:`Path`
+        Returns the path to the new file.
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if the package could not be found.
+    FileExistsError
+        Raised if the file already exists in the package.
+    """
+    # try to get the package
+    try:
+        package_dir = get_package(package_name)
+    except FileNotFoundError:
+        raise FileNotFoundError('Package does not exist')
+    # check if the file already exists
+    file_dir = package_dir.joinpath('files', file_name)
+    if file_dir.is_dir():
+        raise FileExistsError('File already exists')
+    # create the file dir
+    file_dir.mkdir(parents=True)
+    return file_dir
+
--- modrc/tests/lib/test_package.py
+++ modrc/tests/lib/test_package.py
@@ -32,3 +32,60 @@ class TestCreatePackage(unittest.TestCase):
         new_package_dir = package.create_package(new_package_name)
         new_package_yml = new_package_dir.joinpath('package.yml')
         self.assertTrue(new_package_yml.is_file())
+
+
+class TestGetPackage(unittest.TestCase):
+    def setUp(self):
+        # setup a temporary mock home directory
+        self.temp = tempfile.TemporaryDirectory()
+        self.temp_dir = pathlib.Path(self.temp.name)
+        setup.initial_setup(self.temp_dir)
+
+    def tearDown(self):
+        # destroy the ModRC symlink
+        setup.teardown()
+        # destroy the temp directory
+        self.temp.cleanup()
+
+    def test_package_does_not_exist(self):
+        """Test functionality when a package does not exist."""
+        with self.assertRaises(FileNotFoundError):
+            package.get_package('test-package')
+
+    def test_package_exists(self):
+        """Test retrieval of a package."""
+        test_package = package.create_package('test-package')
+        self.assertEqual(test_package, package.get_package('test-package'))
+
+
+class TestCreateFile(unittest.TestCase):
+    def setUp(self):
+        # setup a temporary mock home directory
+        self.temp = tempfile.TemporaryDirectory()
+        self.temp_dir = pathlib.Path(self.temp.name)
+        setup.initial_setup(self.temp_dir)
+
+    def tearDown(self):
+        # destroy the ModRC symlink
+        setup.teardown()
+        # destroy the temp directory
+        self.temp.cleanup()
+
+    def test_package_does_not_exist(self):
+        """Test that an exception is raised when the package does not exist."""
+        with self.assertRaises(FileNotFoundError):
+            package.create_file('test-file', 'test-package')
+
+    def test_file_already_exists(self):
+        """Test that an exception is raised when the file already exists."""
+        package_dir = package.create_package('test-package')
+        package_dir.joinpath('files', 'test-file').mkdir(parents=True)
+        with self.assertRaises(FileExistsError):
+            package.create_file('test-file', 'test-package')
+
+    def test_create_file_success(self):
+        """Tests that the new file is created successfully."""
+        package_dir = package.create_package('test-package')
+        file_dir = package.create_file('test-file', 'test-package')
+        self.assertEqual(package_dir.joinpath('files', 'test-file'), file_dir)
+        self.assertTrue(file_dir.is_dir())
