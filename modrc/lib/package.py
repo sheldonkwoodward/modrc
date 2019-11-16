fb23b2876c4207106d9200ebed93e5295f825641
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-11-16 15:54:47 -0800

    Added package method to get a package.yml file. Updated the create_package() method to optionally add a repo URL to its package.yml on creation.

--- modrc/lib/package.py
+++ modrc/lib/package.py
@@ -1,14 +1,18 @@
+import yaml
+
 from modrc import exceptions
 from modrc.lib import helper
 
 
-def create_package(package_name):
+def create_package(package_name, repo_url=None):
     """Create a new ModRC package.
 
     Parameters
     ----------
     package_name : str
         The name of the new package.
+    repo_url : str
+        The URL of the packages repository.
 
     Returns
     -------
@@ -28,6 +32,17 @@ def create_package(package_name):
     # create the package directories and files
     new_package_dir.mkdir()
     new_package_yml.touch()
+    # add the repo url to package.yml if it was passed into the method
+    if repo_url is not None:
+        package_file = new_package_dir.joinpath('package.yml')
+        with open(str(package_file), 'r') as yf:
+            package_yaml = yaml.safe_load(yf)
+            if package_yaml is None:
+                package_yaml = {}
+        package_yaml['repourl'] = repo_url
+        # write to the package.yml
+        with open(str(package_file), 'w') as yf:
+            yaml.safe_dump(package_yaml, yf, default_flow_style=False)
     return new_package_dir
 
 def get_package(package_name):
@@ -55,3 +70,29 @@ def get_package(package_name):
     if not new_package_dir.is_dir():
         raise exceptions.ModRCPackageDoesNotExistError('Package does not exist')
     return new_package_dir
+
+def get_package_file(package_name):
+    """Get a package.yml file by package name.
+
+    Parameters
+    ----------
+    package_name : str
+        The name of the package to the package.yml file from.
+
+    Returns
+    -------
+    :obj:`Path`
+        The path to the package.yml file.
+
+    Raises
+    ------
+    ModRCIntegrityError
+        Raised if the packages directory does not exist.
+    ModRCPackageDoesNotExistError
+        Raised if the package or package.yml file does not exist.
+    """
+    package_dir = get_package(package_name)
+    package_file = package_dir.joinpath('package.yml')
+    if not package_file.is_file():
+        raise exceptions.ModRCPackageDoesNotExistError
+    return package_file
--- modrc/tests/lib/test_package.py
+++ modrc/tests/lib/test_package.py
@@ -1,6 +1,7 @@
 import pathlib
 import tempfile
 import unittest
+import yaml
 
 from modrc import exceptions
 from modrc.lib import helper, package, setup
@@ -28,11 +29,19 @@ class TestCreatePackage(unittest.TestCase):
         self.assertTrue(new_package_dir.exists())
 
     def test_create_package_yml(self):
-        """Test the creation of the package.yml file"""
-        new_package_name = 'test-package'
-        new_package_dir = package.create_package(new_package_name)
+        """Test the creation of the package.yml file."""
+        new_package_dir = package.create_package('test-package')
         new_package_yml = new_package_dir.joinpath('package.yml')
         self.assertTrue(new_package_yml.is_file())
+
+    def test_add_repo_url(self):
+        """Test that the repo URL is added to the package.yml file."""
+        package.create_package('test-package', 'https://url.example')
+        package_file = package.get_package_file('test-package')
+        with open(str(package_file), 'r') as yf:
+            package_yaml = yaml.safe_load(yf)
+        self.assertIn('repourl', package_yaml)
+        self.assertEqual(package_yaml['repourl'], 'https://url.example')
 
 
 class TestGetPackage(unittest.TestCase):
@@ -57,3 +66,36 @@ class TestGetPackage(unittest.TestCase):
         """Test retrieval of a package."""
         test_package = package.create_package('test-package')
         self.assertEqual(test_package, package.get_package('test-package'))
+
+
+class TestGetPackageFile(unittest.TestCase):
+    def setUp(self):
+        # setup a temporary mock home directory
+        self.temp = tempfile.TemporaryDirectory()
+        self.temp_dir = pathlib.Path(self.temp.name)
+        setup.initial_setup(self.temp_dir)
+
+    def tearDown(self):
+        # destroy the ModRC symlink
+        setup.teardown(ignore_errors=True)
+        # destroy the temp directory
+        self.temp.cleanup()
+
+    def test_package_dir_does_not_exist(self):
+        """Test functionality when a package does not exist."""
+        with self.assertRaises(exceptions.ModRCPackageDoesNotExistError):
+            package.get_package_file('test-package')
+
+    def test_package_file_does_not_exist(self):
+        """Test functionality when a package file does not exist."""
+        packages_dir = helper.get_packages_dir()
+        packages_dir.joinpath('test-package').mkdir()
+        with self.assertRaises(exceptions.ModRCPackageDoesNotExistError):
+            package.get_package_file('test-package')
+
+    def test_package_exists(self):
+        """Test that the package.yml file is retrieved successfully."""
+        package_dir = package.create_package('test-package')
+        package_file = package.get_package_file('test-package')
+        self.assertEqual(package_dir.joinpath('package.yml'), package_file)
+        self.assertTrue(package_file.is_file())
