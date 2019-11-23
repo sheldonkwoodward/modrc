683c9c0bf73b3145263a98bc7bc55bf328c808da
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-11-23 13:34:32 -0800

    Moved tests to their own separate directory.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 docker build -f test.Dockerfile -t modrc_tests:latest .
-docker run --name modrc_tests -v $(pwd)/modrc/:/modrc/modrc modrc_tests:latest python -m unittest discover
+docker run --name modrc_tests -v $(pwd)/modrc/:/modrc/modrc -v $(pwd)/tests/:/modrc/tests modrc_tests:latest python -m unittest discover
 docker rm modrc_tests
--- test.Dockerfile
+++ test.Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.8-alpine
 RUN mkdir -p /modrc/modrc
+RUN mkdir -p /modrc/tests
 WORKDIR /modrc
 COPY ./requirements.txt ./requirements.txt
 RUN pip install -r requirements.txt
