f1e0af898b12aab92543d57570232732c9130bb8
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-12-25 19:58:10 -0800

    Implemented package install command.

--- modrc/commands/package.py
+++ modrc/commands/package.py
@@ -15,6 +15,7 @@ def package():
 @click.option('-d', '--default', 'default', is_flag=True, help='Set the new package as the default package.')
 @click.option('--url', 'repo_url', help='The URL to a package repo, should be an empty repo.')
 def add(name, default, repo_url):
+    """Create a new ModRC package."""
     try:
         modrc_package.create_package(name, repo_url=repo_url, default=default)
     except exceptions.ModRCIntegrityError:
@@ -24,3 +25,21 @@ def add(name, default, repo_url):
         click.secho('Package {} already exists'.format(name), fg='red', bold=True)
         sys.exit(2)
     click.echo("Package {} has been created".format(name))
+
+@package.command()
+@click.argument('url')
+@click.option('-d', '--default', 'default', is_flag=True, help='Set the package as the default package.')
+def install(url, default):
+    """Install an existing ModRC package with Git."""
+    try:
+        modrc_package.install_package(url, default=default)
+    except exceptions.ModRCIntegrityError:
+        click.secho('ModRC is not installed', fg='red', bold=True)
+        sys.exit(2)
+    except exceptions.ModRCPackageExistsError:
+        click.secho('Package {} already exists'.format(url), fg='red', bold=True)
+        sys.exit(2)
+    except exceptions.ModRCGitError:
+        click.secho('Package name could not be inferred, check your URL', fg='red', bold=True)
+        sys.exit(2)
+    click.echo("Package {} has been installed".format(url))
--- tests/commands/test_package.py
+++ tests/commands/test_package.py
@@ -9,15 +9,6 @@ from modrc.lib import helper
 
 
 class TestAddNonInteractive:
-    @pytest.mark.usefixtures('setup_teardown')
-    @pytest.mark.usefixtures('click_runner')
-    def test_package_create(self, click_runner):
-        """Test that package_create is called and was successful."""
-        with mock.patch('modrc.lib.package.create_package', return_value=helper.get_packages_dir().joinpath('test-package')) as create_package:
-            result = click_runner.invoke(__main__.main, ['package', 'add', 'test-package'])
-        create_package.assert_called_once_with('test-package', repo_url=None, default=False)
-        assert result.exit_code == 0
-
     @pytest.mark.usefixtures('click_runner')
     def test_modrc_not_installed(self, click_runner):
         """Test that a warning is shown if ModRC is not installed."""
@@ -34,6 +25,15 @@ class TestAddNonInteractive:
             result = click_runner.invoke(__main__.main, ['package', 'add', 'test-package'])
         create_package.assert_called_once_with('test-package', repo_url=None, default=False)
         assert result.exit_code == 2
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_create(self, click_runner):
+        """Test that package_create is called and was successful."""
+        with mock.patch('modrc.lib.package.create_package', return_value=helper.get_packages_dir().joinpath('test-package')) as create_package:
+            result = click_runner.invoke(__main__.main, ['package', 'add', 'test-package'])
+        create_package.assert_called_once_with('test-package', repo_url=None, default=False)
+        assert result.exit_code == 0
 
     @pytest.mark.usefixtures('setup_teardown')
     @pytest.mark.usefixtures('click_runner')
@@ -60,4 +60,60 @@ class TestAddNonInteractive:
         with mock.patch('modrc.lib.package.create_package', return_value=helper.get_packages_dir().joinpath('test-package')) as create_package:
             result = click_runner.invoke(__main__.main, ['package', 'add', '--url', 'git@github.com:test/test.git', 'test-package'])
         create_package.assert_called_once_with('test-package', repo_url='git@github.com:test/test.git', default=False)
+        assert result.exit_code == 0
+
+
+class TestInstallNonInteractive:
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_modrc_not_installed(self, click_runner):
+        """Test that a warning is shown if ModRC is not installed."""
+        with mock.patch('modrc.lib.package.install_package', side_effect=exceptions.ModRCIntegrityError()) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', 'git@github.com:test/test-package.git'])
+        install_package.assert_called_once_with('git@github.com:test/test-package.git', default=False)
+        assert result.exit_code == 2
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_already_exists(self, click_runner):
+        """Test that an existing package with the same name is detected."""
+        with mock.patch('modrc.lib.package.install_package', side_effect=exceptions.ModRCPackageExistsError()) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', 'git@github.com:test/test-package.git'])
+        install_package.assert_called_once_with('git@github.com:test/test-package.git', default=False)
+        assert result.exit_code == 2
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_name_unknown(self, click_runner):
+        """Test that a URL with an unknwn package name is detected."""
+        with mock.patch('modrc.lib.package.install_package', side_effect=exceptions.ModRCGitError()) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', 'git@github.com:test.git'])
+        install_package.assert_called_once_with('git@github.com:test.git', default=False)
+        assert result.exit_code == 2
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_install(self, click_runner):
+        """Test that install_package is called and was successful."""
+        with mock.patch('modrc.lib.package.install_package', return_value=helper.get_packages_dir().joinpath('test-package')) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', 'git@github.com:test/test-package.git'])
+        install_package.assert_called_once_with('git@github.com:test/test-package.git', default=False)
+        assert result.exit_code == 0
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_default(self, click_runner):
+        """Test that the package is set as default if the flag is passed."""
+        with mock.patch('modrc.lib.package.install_package', return_value=helper.get_packages_dir().joinpath('test-package')) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', '--default', 'git@github.com:test/test-package.git'])
+        install_package.assert_called_once_with('git@github.com:test/test-package.git', default=True)
+        assert result.exit_code == 0
+
+    @pytest.mark.usefixtures('setup_teardown')
+    @pytest.mark.usefixtures('click_runner')
+    def test_package_non_default(self, click_runner):
+        """Test that the package is not set as default if the flag is passed."""
+        with mock.patch('modrc.lib.package.install_package', return_value=helper.get_packages_dir().joinpath('test-package')) as install_package:
+            result = click_runner.invoke(__main__.main, ['package', 'install', 'git@github.com:test/test-package.git'])
+        install_package.assert_called_once_with('git@github.com:test/test-package.git', default=False)
         assert result.exit_code == 0
